0526f6ed612d9c109311ce45683b9dc1684bcb95
seweryn <seweryq@gmail.com>
2026-06-07 22:18:59 +0200

    feat: added reservations in employee panel index file

--- src/Repository/ReservationRepository.php
+++ src/Repository/ReservationRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Entity\Cinema;
 use App\Entity\Reservation;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
@@ -26,6 +27,20 @@ class ReservationRepository extends ServiceEntityRepository
             ->join('s.hall', 'h')
             ->andWhere('r.user = :user')
             ->setParameter('user', $user)
+            ->orderBy('s.startsAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findByCinemaOrderedByNewest(Cinema $cinema): array
+    {
+        return $this->createQueryBuilder('r')
+            ->join('r.screening', 's')
+            ->join('s.movie', 'm')
+            ->join('s.hall', 'h')
+            ->join('r.user', 'u')
+            ->andWhere('s.cinema = :cinema')
+            ->setParameter('cinema', $cinema)
             ->orderBy('s.startsAt', 'DESC')
             ->getQuery()
             ->getResult();
